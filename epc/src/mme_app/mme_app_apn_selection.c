3114783881ec31d77d0a56ea4655f9343889170b
Spencer Sevilla <sevilla@cs.washington.edu>
2018-10-30 15:47:07 -0700

    bstr fixes

--- epc/src/mme_app/mme_app_apn_selection.c
+++ epc/src/mme_app/mme_app_apn_selection.c
@@ -58,7 +58,7 @@ struct apn_configuration_s   * mme_app_select_apn(ue_mm_context_t * const ue_con
   if (!ue_selected_apn) {
     OAILOG_DEBUG(LOG_MME_APP, "SMS: ue_selected_apn=NULL\n", ue_selected_apn);
   } else {
-    OAILOG_DEBUG(LOG_MME_APP, "SMS: ue_selected_apn=%s\n, len=%u", ue_selected_apn, ue_selected_apn->slen);
+    OAILOG_DEBUG(LOG_MME_APP, "SMS: ue_selected_apn=%s\n, len=%u\n", ue_selected_apn->data, ue_selected_apn->slen);
   }
 
   for (index = 0; index < ue_context->apn_config_profile.nb_apns; index++) {
